52de1371d05748bbf81da1d602ca1f66f3a2bfff
Ian Homer <ihomer@worth.systems>
2020-07-01 22:30:12 +0100

    Add components

---
+++ components/SiteLayout.js
@@ -0,0 +1,45 @@
+import Head from "next/head";
+import Link from "next/link";
+
+const SiteLayout = ({ children }) => (
+  <div className="container">
+    <Head>
+      <title>Welcome</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+
+    <h1>Welcome to Worth Engineering</h1>
+    <div>{children}</div>
+           <ul className="home">
+          <li>
+            <Link href="/launch/ignition">
+              <a>Ignition</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/launch/lift-off">
+              <a>Lift off!</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/launch/stage-one-seperation">
+              <a>Stage 1 Seperation</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/launch/stage-two-seperation">
+              <a>Stage 2 Seperation</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/launch/orbital">
+              <a>Orbital</a>
+            </Link>
+          </li>
+        </ul>
+
+
+  </div>
+)
+
+export default SiteLayout
