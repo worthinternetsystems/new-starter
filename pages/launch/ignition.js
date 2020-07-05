c7d64363d0700e1f277ebb9dc2af961351b19690
Shaun Bulbrook <sbulbrook@worth.systems>
2020-07-05 18:08:55 +0100

    Copy sweep on ignition

--- pages/launch/ignition.js
+++ pages/launch/ignition.js
@@ -2,87 +2,134 @@ export default function Home() {
   return (
     <main>
       <h2>Ignition</h2>
-      <p>Hello. 🤗</p>
       <p>
-        We just want to make sure that you have access and you've gotten
-        onto slack and connected to channels
-        like #worth-reading, #tech and other things that float your boat like
-        ⛄️ #snowfeverworth and #food.
+        <strong><em>Hello!!!</em></strong>
+        {' '}
+        <span role="img" aria-label="">👋🚀🙌🤗</span>
       </p>
       <p>
-        We'll help you get to know the project that you're on, but don't worry
+        We just want to make sure that you have access to anything you might need, you&apos;ve gotten
+        onto slack and connected to channels
+        like
+        {' '}
+        <a href="https://worthsystems.slack.com/archives/GBR1XHMDW">#worth-reading</a>
+        ,
+        {' '}
+        <a href="https://worthsystems.slack.com/archives/C90RZ5XFU">#tech</a>
+        {' '}
+        and other things that float your boat like
+        {' '}
+        <a href="https://worthsystems.slack.com/archives/CEE76EC85">️#snowfeverworth</a>
+        {' '}
+        and
+        {' '}
+        <a href="https://worthsystems.slack.com/archives/C4W2DTAMS">#funfoodfridayforyou</a>
+        .
+      </p>
+      <p>
+        We&apos;ll help you get to know the project that you&apos;re on, but don&apos;t worry
         about that right now!
       </p>
       <p>
         Where do
+        {' '}
         <em>you</em>
-        want to go? We'll be talking to you about
-        progression and lots of other things! But again don't worry about that
+        {' '}
+        want to go? We&apos;ll be talking to you about
+        progression and lots of other things! But again don&apos;t worry about that
         yet. 🙂
       </p>
+      <p>
+        <ul>
+          <li>
 
-      <ul>
-        <li>
-          Let's check the basic, have you got into you email account and slack.
-          <ul>
-            <li>
-              Slack
-              <ul>
-                <li>#worth-reading</li>
-                <li>#tech</li>
-                <li>#fridayguild</li>
-                <li>#readingbookclub</li>
-                <li>#snowfeverworth</li>
-              </ul>
-              Search for more and join in the conversation. It's a great way to get involved.
-           </li>
-            <li>
-              You should have received a few calendar invites
-              <ul>
-                <li>Thirsty Thursday</li>
-                <li>Timetastic</li>
-                <li>Bi-weekly Meeting</li>
-              </ul>
-              ... if your calendar is empty then say something.
-            </li>
-          </ul>
-        </li>
-      </ul>
+            Let&apos;s check the basics, have you got into your
+            {' '}
+            <a href="http://mail.google.com">email account</a>
+            {' '}
+            and
+            {' '}
+            <a href="https://worthsystems.slack.com">slack</a>
+            ?
+            <ul>
+              <li>
+                Slack
+                <ul>
+                  <li><a href="https://worthsystems.slack.com/archives/GBR1XHMDW">worth-reading</a></li>
+                  <li><a href="https://worthsystems.slack.com/archives/C90RZ5XFU">#tech</a></li>
+                  <li><a href="https://worthsystems.slack.com/archives/CT2KUAG77">#fridayguild</a></li>
+                  <li><a href="https://worthsystems.slack.com/archives/CSVNKPNN4">#readingbookclub</a></li>
+                  <li><a href="https://worthsystems.slack.com/archives/C4E3B71NW">#praise</a></li>
+                  <li>Search for more and join in the conversation. It&apos;s a great way to get involved!</li>
+                </ul>
+              </li>
+              <li>
+                You should have received a few
+                {' '}
+                <a href="https://calendar.google.com/">calendar</a>
+                {' '}
+                invites
+                <ul>
+                  <li>Thirsty Thursday</li>
+                  <li>Timetastic</li>
+                  <li>Bi-weekly Meeting</li>
+                </ul>
+                ... if your calendar is empty then
+                {' '}
+                <em>say something</em>
+                !
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </p>
 
       <p>
-        As you step through your first days, weeks and months at WORTH, we've
+        As you step through your first days, weeks and months at WORTH, we&apos;ve
         put together
         a few RACI check lists, to indicate what you are
-        &nbsp;<strong>R</strong>
+        &nbsp;
+        <strong>R</strong>
         esponsible for,
         what
         you are
-        &nbsp;<b>A</b>
+        &nbsp;
+        <b>A</b>
         ccountable for, what you should be
-        &nbsp;<b>C</b>
+        &nbsp;
+        <b>C</b>
         onsulted about
         and what you
         should be
-        &nbsp;<b>I</b>
+        &nbsp;
+        <b>I</b>
         nformed about. Come back to these as you ramp up and
-        make sure you're
-        on track. Ask for help if some of them aren't clear.
+        make sure you&apos;re
+        on track. Ask for help if some of them aren&apos;t clear.
       </p>
 
       <ul className="raci">
         <li className="responsible">
-          Get yourself comfortable 🧶
+          Get yourself comfortable
+          {' '}
+          <span role="img" aria-label="">🧶</span>
         </li>
         <li className="accountable">
-          Saying hello on slack, make yourself known👀
+          Saying hello on slack, make yourself known!
+          {' '}
+          <span role="img" aria-label="">👀</span>
         </li>
         <li className="consulted">
           Have people asked if you’re OK? Have you been asked
-          what your T-Shirt size is🦻
+          what your T-Shirt size is?
+          {' '}
+          <span role="img" aria-label="">🦻</span>
         </li>
         <li className="informed">
           Have you got your welcome box? Welcome email? some
-          calendar invites? 📅
+          calendar invites?
+          {' '}
+          <span role="img" aria-label="">📅</span>
         </li>
       </ul>
 
@@ -91,26 +138,79 @@ export default function Home() {
         environment configured just the way you like it and exploring some
         getting started guides.
       </p>
-        <p>Tools we like:</p>
-        <ul>
-          <li>Brew (You can use this to quickly get the others!)</li>
-          <li>VSCode/IntelliJ</li>
-          <li>Fish/ZSH</li>
-          <li>nano/vi</li>
-          <li>Git</li>
-          <li>Zoom</li>
-          <li>DrawIO</li>
-        </ul>
       <p>
-        Tech we currently like include
+        Tools we like:
+        <ul>
+          <li><a href="https://brew.sh/">Brew (You can use this to quickly get the others!)</a></li>
+          <li>
+            <a href="https://code.visualstudio.com/">VSCode/IntelliJ</a>
+            {' '}
+            <code>brew cask install visual-studio-code</code>
+          </li>
+          <li>
+            <a href="https://fishshell.com/">Fish</a>
+            /
+            <a href="https://www.zsh.org/">ZSH</a>
+            {' '}
+            <code>brew install fish</code>
+            {' '}
+            /
+            {' '}
+            <code>brew install zsh</code>
+            {' '}
+          </li>
+          <li>
+            <a href="https://www.nano-editor.org/">nano</a>
+            /
+            <a href="https://www.vim.org/">vim</a>
+            {' '}
+            <code>brew install nano</code>
+            {' '}
+            /
+            {' '}
+            <code>brew install vim</code>
+          </li>
+          <li>
+            <a href="https://git-scm.com/">Git</a>
+            {' '}
+            <code>brew install git</code>
+          </li>
+          <li>
+            <a href="https://zoom.us/">Zoom</a>
+            {' '}
+            <code>brew cask install zoom</code>
+          </li>
+          <li>
+            <a href="https://app.diagrams.net/">Diagrams</a>
+            {' '}
+            <code>brew cask install drawio</code>
+          </li>
+        </ul>
       </p>
-      <ul>
-        <li>Node, Java & C#</li>
-        <li>React (inc Native)</li>
-        <li>Serverless Framework</li>
-        <li>Docker</li>
-        <li>Terraform</li>
-      </ul>
-   </main>
+      <p>
+        Tech we currently like include:
+      </p>
+      <p>
+        <ul>
+          <li>
+            <a href="https://nodejs.org/en/">Node</a>
+            , Java &
+            {' '}
+            <a href="https://docs.microsoft.com/en-us/dotnet/csharp/">C#</a>
+          </li>
+          <li>
+            <a href="https://reactjs.org/">React</a>
+            {' '}
+            (inc
+            {' '}
+            <a href="https://reactnative.dev/">Native</a>
+            )
+          </li>
+          <li><a href="https://www.serverless.com/">Serverless Framework</a></li>
+          <li><a href="https://www.docker.com/">Docker</a></li>
+          <li><a href="https://www.terraform.io/">Terraform</a></li>
+        </ul>
+      </p>
+    </main>
   );
 }
