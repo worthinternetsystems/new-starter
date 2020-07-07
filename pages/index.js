624ce79bd483cb45b48081bc73e590d43b8d5251
Henry Penton <hpenton@worth.systems>
2020-07-07 21:21:05 +0100

    rocket :)

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import Link from "next/link";
+import Link from 'next/link';
 
 export default function Home() {
   return (
@@ -14,6 +14,35 @@ export default function Home() {
           </Link>
         </li>
       </ul>
+      <div className="rocket">
+        <div className="rocket-body">
+          <div className="body" />
+          <div className="fin fin-left" />
+          <div className="fin fin-right" />
+          <div className="window" />
+        </div>
+        <div className="exhaust-flame" />
+        <ul className="exhaust-fumes">
+          <li />
+          <li />
+          <li />
+          <li />
+          <li />
+          <li />
+          <li />
+          <li />
+          <li />
+        </ul>
+        <ul className="star">
+          <li />
+          <li />
+          <li />
+          <li />
+          <li />
+          <li />
+          <li />
+        </ul>
+      </div>
     </main>
   );
 }
